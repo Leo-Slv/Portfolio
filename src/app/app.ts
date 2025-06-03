4b3c3516794e28201b013b105511eac4b2ba8789
Leo-Slv <leonardo1692004@gmail.com>
2025-06-03 00:40:49 -0300

    chore: add tailwind css and flowbite css as dependency

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected title = 'Portfolio';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  
+   ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      initFlowbite();
+    }
+  }
 }
